76e5d401e386aece22d8a1511d287780f2d3018a
Баюн Михайло <t-baiunm@mon.gov.ua>
2025-04-05 23:30:48 +0300

    change style

--- js/main.js
+++ js/main.js
@@ -5,26 +5,102 @@ async function getData(date) {
     try {
         const response = await fetch(url)
         const data = await response.json()
+        document.getElementById('info').textContent = `Курси на дату: ${mathDate}`
+
+        document.getElementById('convertBtn').addEventListener('click', () => {
+            const fromCurrency = document.getElementById('fromCurrencyInput').value.toUpperCase()
+            const toCurrency = document.getElementById('toCurrencyInput').value.toUpperCase()
+            const suma = document.getElementById('suma').value
+
+            const fromCurrencyData = data.exchangeRate.find(rate => rate.currency === fromCurrency);
+            const toCurrencyData = data.exchangeRate.find(rate => rate.currency === toCurrency);
+
+            if (!fromCurrencyData || !toCurrencyData) {
+                document.getElementById('result').textContent = 'Введені валюти не знайдено.';
+                return;
+            }
+
+            const fromRate = fromCurrencyData.saleRateNB;
+            const toRate = toCurrencyData.purchaseRateNB;
+
+            const convertedSuma = (suma / toRate) * fromRate;
+
+            document.getElementById('result').textContent = `Результат: ${convertedSuma.toFixed(2)}`;
+        })
 
         const table = document.querySelector('table')
         table.innerHTML = `
                 <tr>
-                    <th>baseCurrency</th>
-                    <th>currency</th>
-                    <th>saleRateNB</th>
-                    <th>purchaseRateNB</th>
+                    <th>Базова валюта</th>
+                    <th>Валюта для обміну</th>
+                    <th>Курс продажу</th>
+                    <th>Курс купівлі</th>
                 </tr>
             `
 
+        const filterCurrency = document.getElementById('filterCurrency').value
         data.exchangeRate.forEach(element => {
-            const tr = document.createElement('tr')
-            tr.innerHTML = `
-            <td>${element.baseCurrency}</td>
-            <td>${element.currency}</td>
-            <td>${element.saleRateNB}</td>
-            <td>${element.purchaseRateNB}</td>
-            `
-            table.appendChild(tr)
+            if (filterCurrency === 'all' || element.currency === filterCurrency) {
+                const tr = document.createElement('tr')
+                tr.innerHTML = `
+                <td>${element.baseCurrency}</td>
+                <td>${element.currency}</td>
+                <td>${element.saleRateNB}</td>
+                <td>${element.purchaseRateNB}</td>
+                `   
+                table.appendChild(tr)
+            } else 
+            if (filterCurrency === 'USD' && element.currency === 'USD') {
+                const tr = document.createElement('tr')
+                tr.innerHTML = `
+                <td>${element.baseCurrency}</td>
+                <td>${element.currency}</td>
+                <td>${element.saleRateNB}</td>
+                <td>${element.purchaseRateNB}</td>
+                `   
+                table.appendChild(tr)
+            } else 
+            if (filterCurrency === 'EUR' && element.currency === 'EUR') {
+                const tr = document.createElement('tr')
+                tr.innerHTML = `
+                <td>${element.baseCurrency}</td>
+                <td>${element.currency}</td>
+                <td>${element.saleRateNB}</td>
+                <td>${element.purchaseRateNB}</td>
+                `   
+                table.appendChild(tr)
+            } else 
+            if (filterCurrency === 'PLN' && element.currency === 'PLN') {
+                const tr = document.createElement('tr')
+                tr.innerHTML = `
+                <td>${element.baseCurrency}</td>
+                <td>${element.currency}</td>
+                <td>${element.saleRateNB}</td>
+                <td>${element.purchaseRateNB}</td>
+                `   
+                table.appendChild(tr)
+            } else 
+            if (filterCurrency === 'CZK' && element.currency === 'CZK') {
+                const tr = document.createElement('tr')
+                tr.innerHTML = `
+                <td>${element.baseCurrency}</td>
+                <td>${element.currency}</td>
+                <td>${element.saleRateNB}</td>
+                <td>${element.purchaseRateNB}</td>
+                `   
+                table.appendChild(tr)
+            } else 
+            if (filterCurrency === 'allFamous' && 
+                (element.currency === 'USD' || element.currency === 'EUR' || element.currency === 'PLN' || element.currency === 'CZK')) {
+                const tr = document.createElement('tr')
+                tr.innerHTML = `
+                <td>${element.baseCurrency}</td>
+                <td>${element.currency}</td>
+                <td>${element.saleRateNB}</td>
+                <td>${element.purchaseRateNB}</td>
+                `   
+                table.appendChild(tr)
+            }
         });
     } catch (error) {
         console.error(error)
